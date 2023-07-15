fb25108e140a856f0fec7551cb7dde5f8a5b8140
Mario <canalessanchezmario@gmail.com>
2023-07-15 16:50:38 +0200

    Se añade un link a los posts individuales

--- app/posts/[id]/ListOfPosts.jsx
+++ app/posts/[id]/ListOfPosts.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { LikeButton } from './LikeButton.jsx'
+import Link from 'next/link.js'
 const fetchPosts = () => {
-  return fetch('https://jsonplaceholder.typicode.com/posts')
+  return fetch('https://jsonplaceholder.typicode.com/posts', {
+    next: 120000
+  })
     .then((res) => res.json())
 }
 
@@ -10,7 +13,9 @@ export async function ListOfPosts () {
 
   return posts.slice(0, 5).map((post) => (
     <article key={post.id}>
-      <h1 style={{ color: '#E5C136' }}>{post.title}</h1>
+      <Link href="/posts/[id]" as={`./posts/${post.id}`}>
+        <h1 style={{ color: "#E5C136" }}>{post.title}</h1>
+      </Link>
       <p>{post.body}</p>
       <LikeButton id={post.id}>Me gusta</LikeButton>
     </article>
